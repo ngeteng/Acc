c4ef92e6247baa46732ee5eb97c8ee6073cf9828
＾Izumi Wakanabe＾ <38299646+0x062@users.noreply.github.com>
2025-06-14 00:52:22 +0700

    Update MyToken.sol

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -4,7 +4,14 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    constructor() ERC20("My First Token", "MFT") {
-        _mint(msg.sender, 1000000 * 10 ** decimals());
+    // Constructor sekarang menerima nama, simbol, dan suplai awal
+    constructor(
+        string memory name, 
+        string memory symbol, 
+        uint256 initialSupply
+    ) ERC20(name, symbol) {
+        // Mint 'initialSupply' token ke alamat yang mendeploy
+        // Kita kalikan dengan 10**18 karena ERC20 punya 18 desimal
+        _mint(msg.sender, initialSupply * 10 ** decimals());
     }
 }
