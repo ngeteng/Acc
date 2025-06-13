a3105446d5076d5fd4eda06eaf8ebc7051b6a8aa
＾Izumi Wakanabe＾ <38299646+0x062@users.noreply.github.com>
2025-06-14 00:55:10 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,12 +5,6 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.20",
   networks: {
-    Pharos: {
-      url: process.env.PHAROS_RPC_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-      chainId: 688688
-    },
     Somnia: {
       url: process.env.SOMNIA_RPC_URL || "",
       accounts:
