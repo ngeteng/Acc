c9da0f300b74b11c16ab76cf1d8a512635926828
＾Izumi Wakanabe＾ <38299646+0x062@users.noreply.github.com>
2025-06-14 00:25:07 +0700

    Update MyNFT.sol

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -1,9 +1,11 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyNFT is ERC721, Ownable {
+contract MyNFT is ERC721, Ownable { // <== BUKA KONTRAK
+
     // State variables
     private uint256 _nextTokenId;
     uint256 public immutable maxSupply;
@@ -22,3 +24,5 @@ contract MyNFT is ERC721, Ownable {
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
     }
+
+} // <== TUTUP KONTRAK (YANG INI HILANG DI FILE-MU)
