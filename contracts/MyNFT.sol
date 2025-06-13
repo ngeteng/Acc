9ae076d6bdd15a03311d13d081416c18c50610c7
＾Izumi Wakanabe＾ <38299646+0x062@users.noreply.github.com>
2025-06-13 23:48:11 +0700

    Create MyNFT.sol

---
+++ contracts/MyNFT.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721, Ownable {
+    private uint256 _nextTokenId;
+    uint256 public immutable maxSupply;
+
+    constructor(string memory name, string memory symbol, uint256 _maxSupply) 
+        ERC721(name, symbol)
+        Ownable(msg.sender) 
+    {
+        maxSupply = _maxSupply;
+    }
+
+    function safeMint(address to) public onlyOwner {
+        require(_nextTokenId < maxSupply, "All tokens have been minted");
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+    }
+}
