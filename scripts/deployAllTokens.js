03b6c5b1f1f2d7fda9caac27e6779d673e31d95b
＾Izumi Wakanabe＾ <38299646+0x062@users.noreply.github.com>
2025-07-04 18:02:06 +0700

    Update deployAllTokens.js

--- scripts/deployAllTokens.js
+++ scripts/deployAllTokens.js
@@ -5,7 +5,7 @@ const { sendMessage } = require('./telegramReporter.js'); // Impor reporter kita
 // =============================================================
 // PUSAT KONTROL: TENTUKAN SEMUA JARINGAN TARGET DI SINI
 // =============================================================
-const targetNetworks = ["Pharos", "Somnia", "OG"];
+const targetNetworks = ["Pharos", "Somnia", "Xos", "OG"];
 // =============================================================
 // FUNGSI HELPER
 // =============================================================
