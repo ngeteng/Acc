defc31ccfbe4a42dcb16a4d8fcce5ea272383166
＾Izumi Wakanabe＾ <38299646+0x062@users.noreply.github.com>
2025-07-16 13:16:33 +0700

    Update deployEcosystem.js

--- scripts/deployEcosystem.js
+++ scripts/deployEcosystem.js
@@ -5,7 +5,7 @@ const { sendMessage } = require('./telegramReporter.js');
 // =============================================================
 // KONFIGURASI
 // =============================================================
-const targetNetworks = ["Monad", "Pharos", "Somnia", "OG"];
+const targetNetworks = ["Xos", "Pharos", "Somnia", "OG"];
 
 // =============================================================
 // FUNGSI HELPER
